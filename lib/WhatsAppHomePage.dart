3415568a8d9bdcb7df320265168ed2bda185758c
AliBagherz <bagherzadeh007@gmail.com>
2020-05-14 04:50:50 +0430

    Just a center WhatsApp

---
+++ lib/WhatsAppHomePage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WhatsAppHomePage extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Text('WhatsApp'),
+    );
+  }
+  
+}
